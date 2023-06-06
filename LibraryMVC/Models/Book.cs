c302e46f7559fcbec16cbb532229e5b0863c1eb3
mbonner88 <matthewsbonner@comcast.net>
2023-06-06 15:57:05 -0400

    Create Book model

---
+++ LibraryMVC/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryMVC.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        [DataType(DataType.Date)] 
+        public DateTime PublicationDate { get; set; }
+        [Display(Name = "ISBN")]
+        public int? Isbn { get; set; }
+    }
+}
